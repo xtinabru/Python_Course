1ac49b137c924cfc8ded6c317d069fb4acf6a119
Cristofor4ik <xtinabruscaginxxx@gmail.com>
2024-06-05 22:04:36 +0300

    list a bit

--- lists.py
+++ lists.py
@@ -1,7 +1,22 @@
 a = [11, 45, 6, 2]
+
+
+"""
+
 print(a[1])
 
 #to print the last element (-1, -2, -3 etc...)
 print(a[-1])
+
 # or the limit
 print (a[0:3])
+
+"""
+
+#add elements to the list
+a.append(77)
+print(a)
+
+#delete elemets
+del a[0]
+print(a)
