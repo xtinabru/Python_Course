ef3425c73be6cfc42c87dc6aa09c46582e975962
Cristofor4ik <xtinabruscaginxxx@gmail.com>
2024-06-07 23:49:06 +0300

    some exercises on sep and end

---
+++ sep_and_end.py
@@ -0,0 +1,34 @@
+""" 1st part
+
+# about seps
+print("aa", "bb", "cc", sep="*")
+
+# to use  a variable as a separator
+minus = "-"
+print("aa", "bb", "cc", sep=minus)
+
+# when there are several prints
+print("A great man doesn't seek to lead.")
+print("He's called to it. And he answers.")
+
+minus = "-" #!!!!
+print("first", "second", "third", end=minus)
+print("second line")
+
+"""
+
+"""
+# empty brackets =
+print("Python")
+print()
+print("C#") 
+
+"""
+
+# print("a", "\n", "b", "\n", "c", sep="*", end="#")
+
+# print("a", "b", "c", sep="*", end="finish")
+
+print("31", sep="+")
+
+print("31", "12", "2019", sep="/")
