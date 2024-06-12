f726578672ca27fa1b3d7bbaa47702b7da1eaeb2
Cristofor4ik <xtinabruscaginxxx@gmail.com>
2024-06-13 01:59:43 +0300

    Theory on built in functions

---
+++ data_types/built_in_functions.py
@@ -0,0 +1,36 @@
+# Built in function means that the programmer doesn't see its realization.
+
+# They are:
+# 👽
+# print()
+# input()
+# int()
+# float()
+
+# Functions min() and max() ⭐️
+
+# doesn't matter how many arguments
+# the main point is that all of them should support the comparison operation, for example: float and int
+# but float and str  - do not support
+
+# a = max(3, 8, -3, 12, 9)
+# b = min(3, 8, -3, 12, 9)
+# c = max(3.14, 2.17, 9.8)
+# print(a)
+# print(b)
+# print(c)
+
+# Functions abs() ⭐️
+
+# The absolute value or modulus
+
+# the abs of "+" number = number, if n > 0
+# the abs of "-" number = n, if n < 0
+# the abs of "0" = 0, if n = 0
+
+# print(abs(10))
+# print(abs(-7))
+# print(abs(0))
+# print(abs(-17.67))
+
+# max(), min(), abs() work with BOTH: int and float numbers
