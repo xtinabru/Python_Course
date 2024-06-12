9dbbe87149d1f96736c406e88e1a751a15d6d79d
Cristofor4ik <xtinabruscaginxxx@gmail.com>
2024-06-12 04:06:14 +0300

    Exercises on floats

---
+++ data_types/exercises_float.py
@@ -0,0 +1,61 @@
+# ex 1 S 📐
+
+# S = 1/2 * a * b
+
+# cathetus1, cathetus2 = (
+#     float(input()),
+#     float(input()),
+# )
+# area = 1 / 2 * (cathetus1 * cathetus2)  # or s = a * b / 2
+# print(area)
+
+# ex 2 TWO old ladies 👵👵
+
+# Two old ladies are walking towards each other with constant speeds V1 and V2 km/h. Determine in what time (in hours) the old ladies will meet if the distance between them is equal to S km.
+
+# s = float(input())
+# V1 = float(input())
+# V2 = float(input())
+
+# V_close = V1 + V2
+# time = s / V_close
+
+# print(time)
+
+# ex 3 REVERSE 🌀
+
+# num = float(input())
+
+# if num == 0:
+#     print("Обратного числа не существует")
+# else:
+#     rev = 1 / num
+#     print(rev)
+
+# ex 4 Fahrenheit 451  🌡️
+
+# farenh = float(input())
+
+# celc = 5 / 9 * (farenh - 32)
+# print(celc)
+
+# ex 5 Dog age 🐶
+
+# The input to the programme is a number of dog years. Write a programme that calculates the age of a dog in human years using the following algorithm: for the first two years, a dog year is equal to  10.5 human years, after that each dog year is  4 human years.
+
+# n = int(input())
+
+# if n <= 2:
+#     print((n * 10.5))
+# elif n > 2:
+#     print((n - 2) * 4 + 21)
+
+# ex 6 First digit after .
+
+# numb = float(input())
+
+# print(int((numb * 10) % 10))
+
+# # ex 7
+# numb = float(input())
+# print(numb - int(numb))
