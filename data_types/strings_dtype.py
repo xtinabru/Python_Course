7dde6631c4150879356dc875aebd9bf7a380525b
Cristofor4ik <xtinabruscaginxxx@gmail.com>
2024-06-13 03:59:54 +0300

    Strings theory

---
+++ data_types/strings_dtype.py
@@ -0,0 +1,55 @@
+# Strings 🎀
+
+# examples:
+# s1 = 'Python rocks!'
+# s2 = "Python rocks!"
+
+# by default input() reads string
+
+# s = input()  # переменная s имеет строковый тип str
+
+# for empty string:
+# s1 = ""
+# s2 = " "  📍they are different
+
+# Length 📏
+
+# s1 = "abcdef"
+# length1 = len(s1)
+# print(length1)
+
+# Numbers to str 📈
+
+# num1 = 1777
+# num2 = 17.77
+# s1 = str(num1)
+# s2 = str(num2)
+
+# Concatenation 📎
+
+# s1 = "ab" + "bc"
+# s2 = "bc" + "ab"
+# s3 = s1 + s2 + "!!"
+# print(s1)
+# print(s2)
+# print(s3)
+
+# print("a", "b", "c", sep="*", end="!")
+# print()  # new line
+# print("a" + "*" + "b" + "*" + "c" + "!")
+
+# Division str to num ⚒
+
+# s = "Hi" * 4
+# print(s)
+# print("-" * 75)
+
+# STR CAN NOT BE MULT TO STR❗️
+
+# NOTES
+
+# 🟪 for multuline text: '''
+
+text = """Python is an interpreted, high-level, general-purpose programming language.
+Created by Guido van Rossum and first released in 1991, Python design 
+philosophy emphasizes code readability with its notable use of significant whitespace."""
