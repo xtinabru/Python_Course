82311fa45d1bc229dea969ec5164bb7ed6b002c7
Cristofor4ik <xtinabruscaginxxx@gmail.com>
2024-06-14 21:54:45 +0300

    Area of the circle

--- data_types/math_exercises.py
+++ data_types/math_exercises.py
@@ -1,10 +1,26 @@
-from math import pow, sqrt
+# Ex 1 Euclidean distance 🧡
 
-x1 = float(input())
-y1 = float(input())
-x2 = float(input())
-y2 = float(input())
+# from math import pow, sqrt
+
+# x1 = float(input())
+# y1 = float(input())
+# x2 = float(input())
+# y2 = float(input())
 
 
-result = sqrt(pow(x1 - x2, 2) + pow(y1 - y2, 2))
-print(result)
+# result = sqrt(pow(x1 - x2, 2) + pow(y1 - y2, 2))
+# print(result)
+
+# Ex 2 🩷
+# Write a program to determine the area of ​​a circle and the circumference of a circle at a given radius
+
+from math import pow, pi
+
+R = float(input())
+
+
+S = pi * (pow(R, 2))
+C = 2 * pi * R
+
+print(S)
+print(C)
