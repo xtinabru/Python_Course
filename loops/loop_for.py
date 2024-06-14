cb1ca0bda6d7b67a373f05e203f6ed3458095f90
Cristofor4ik <xtinabruscaginxxx@gmail.com>
2024-06-15 01:43:54 +0300

    Loop for. Theory

---
+++ loops/loop_for.py
@@ -0,0 +1,47 @@
+# FOR 🔆
+
+# 1) counting loops  - for  - cycles repeated a certain number of times
+# 2) conditional loops - while  - cycles that repeat until a specific event occurs
+
+# 🟣 The for loop works great if we know in advance how many iterations we need to do.
+
+# for i in range(10):
+#     print("hello")
+
+# for loop_variable_name in range(number_of_repetitions):
+# code block OR body of the loop
+
+# for i in range(3):
+#     num = int(input())
+#     print("The square:", num * num)
+
+# print("Loop is over")
+
+# 🔵
+
+# print("A")
+# print("B")
+
+# for i in range(5):
+#     print("C")
+#     print("D")
+
+# print("E")
+
+# 🟢
+
+# print("A")
+# print("B")
+
+# for i in range(5):
+#     print("C")
+
+# for i in range(5):
+#     print("D")
+
+# print("E")
+
+
+# 🟡🟡🟡
+
+# Executing the body of a loop once is called a loop iteration.
