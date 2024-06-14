e6116257900a3cabc208ca05b666c1d58076b54a
Cristofor4ik <xtinabruscaginxxx@gmail.com>
2024-06-14 20:45:18 +0300

    Theory on MATH modulus

---
+++ data_types/math_theory.py
@@ -0,0 +1,37 @@
+# MATH MODULUS
+
+# to add = import
+
+# import math
+
+# num1 = math.sqrt(2)
+# num2 = math.ceil(3.8)  # округлить вверх
+# num3 = math.floor(3.8)  # округлить вниз
+
+# print(num1)
+# print(num2)
+# print(num3)
+
+# peculiarities 🍩
+
+# 🍎 We can not WRite the name of modulus by adding =from math inport=
+# example:
+
+# from math import *
+
+# num1 = sqrt(2)
+# num2 = ceil(3.8)
+# num3 = floor(3.8)
+# print(num1)
+# print(num2)
+# print(num3)
+
+# 🍎 if we need just several functions of the module,
+# we can import ONLY them:
+
+# from math import sqrt, ceil
+
+# print(sqrt(25))
+# print(ceil(24.5))
+
+# print(floor(12.8))  # приведет к ошибке, так как функция floor не подключена
