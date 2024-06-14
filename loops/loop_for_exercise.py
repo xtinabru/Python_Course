c60d3b43de593c683b29f07600c17b06bc7082ab
Cristofor4ik <xtinabruscaginxxx@gmail.com>
2024-06-15 02:26:13 +0300

    Loop for. Exercises on loop

---
+++ loops/loop_for_exercise.py
@@ -0,0 +1,31 @@
+# Ex 1 Python is awesome! 🐍
+
+# for i in range(10):
+#     print("Python is awesome!")
+
+# Ex 2 "REPEAT AFTER ME" 🐲
+
+# s = input()
+# num = float(input())
+
+# for i in range(int(num)):
+#     print(s)
+
+# Ex 3 Character sequence 🐾
+
+# for i in range(6):
+#     print("A" * 3)
+# for i in range(5):
+#     print("B" * 4)
+# print("E")
+# for i in range(9):
+#     print("T" * 5)
+# print("G")
+
+# Ex 4 Star rectangle 🌟
+
+# t = float(input())
+# n = "*" * 19
+
+# for i in range(int(t)):
+#     print(n)
---
+++ loops/var_loop.py
@@ -0,0 +1,19 @@
+# TO START i from 0:
+
+# for i in range(10):
+#     print(i + 1, "-- Hello")
+
+# var  of loops usually call : i, j, k
+
+# There are situations when a loop variable is not used in the body of the loop. In this case, instead of giving it a name, we can specify the underscore character _ :
+
+# for _ in range(5):
+#     print("Python - awesome!")
+
+# REMEMBER
+
+for i in range(5):
+    print(i)
+
+for i in range(5):
+    print(i + 1)
