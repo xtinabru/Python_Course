306255e3216a5466435ad591ea9da1b7aad12412
Cristofor4ik <xtinabruscaginxxx@gmail.com>
2024-06-14 19:59:36 +0300

    In Theory

---
+++ data_types/in_operator.py
@@ -0,0 +1,27 @@
+# Check whether there is inside 🫧
+
+# s = "https://pygen.ru/"
+
+# if "a" in s:
+#     print("It has a")
+# else:
+#     print("There is not a")
+
+# IN with logical operators 🫧
+
+# s = input()
+# if "." not in "s":
+#     print("there is no DOTS")
+
+
+#### MAKE SHORTER🫧
+# s = input()
+# # if s == "a" or s == "e" or s == "i" or s == "o" or s == "u":
+# #     print("YES")
+# if len(s) == 1 and s in "aeiou":
+#     print("YES")
+
+# s = "Sigma"
+
+# print("a" in s) #TRUE
+# print("z" in s) # FALSE
