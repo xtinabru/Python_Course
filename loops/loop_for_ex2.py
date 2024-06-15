9d1990aa0f3d698e937b6c6d3932a6c8ffa34c25
Cristofor4ik <xtinabruscaginxxx@gmail.com>
2024-06-15 15:13:51 +0300

    Exercises on loops

---
+++ loops/loop_for_ex2.py
@@ -0,0 +1,37 @@
+# Ex 1 "REPEAT AFTER ME" 🐦‍🔥
+
+# Write a program that reads one line of text and outputs 10 lines, numbered 0 to 9, each with the specified line of text.
+
+# s = input()
+
+# for i in range(10):
+#     print(i, s)
+
+# Ex 2 "Number square" 🧊
+
+# num = float(input())
+# for i in range(int(num) + 1):
+#   print("Квадрат числа", i, "равен", i**2)
+
+# Ex 3 Star triangle ⭐️
+
+# n = int(input())
+
+# for i in range(n, 0, -1): # n - начало; 0 - конец; -1 - шаг
+#     print("*" * i)
+
+# Ex 4 Population 👨🏼‍🔬 Compound interest %%%%%%%%%%%
+
+# m: starting number of organisms;
+# p: average daily increase in %
+# n: number of days for reproduction
+
+# x * (1 + a%/100)**n
+
+# m = int(input())
+# p = int(input())
+# n = int(input())
+
+# for i in range(1, n + 1):
+#     print(i, m)
+#     m = m * (1 + p / 100)
