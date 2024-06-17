448f2496a94fdf27cdabbd9357524051148d967d
Cristofor4ik <xtinabruscaginxxx@gmail.com>
2024-06-17 03:01:22 +0300

    Scenarios ex.p2

---
+++ loops/scen_exercise2.py
@@ -0,0 +1,54 @@
+# Ex 1 Amount of numbers 👑
+
+# a = int(input())
+# b = int(input())
+
+# counter = 0
+
+# for i in range(a, b + 1):
+#     cube = i ** 3
+#     if cube % 10 == 4 or cube % 10 == 9:
+#         counter += 1
+
+# print(counter)
+
+# Ex 2 Sum of numbers👑
+
+# n = int(input())
+
+# sum = 0
+
+# for i in range(n):
+#     number = int(input())
+#     sum += number
+
+# print(sum)
+
+# Ex 3 Asymptotic approximation👑
+
+# import math
+
+# n = int(input())
+
+# sum_fractions = 0
+# for i in range(1, n + 1):
+#     sum_fractions += 1 / i
+
+# log_n = math.log(n)
+
+# result = sum_fractions - log_n
+# print(result)
+
+# Ex 4 Sum of numbers 2 🟢
+
+
+# n = int(input())
+
+# total = 0
+
+# for i in range(1, n + 1):
+#     last_digit = (i**2) % 10
+#     if last_digit == 2 or last_digit == 5 or last_digit == 8:
+#         total += i
+
+# print(total)
