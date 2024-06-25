5689a537e2e3f91cbac79076216fae18958c08be
Cristofor4ik <xtinabruscaginxxx@gmail.com>
2024-06-25 23:43:38 +0300

    Continue and break operators. Exercises

---
+++ ex1.py
@@ -0,0 +1,56 @@
+# 🔸 Exercise 1:
+
+# for i in range(10):
+#     print(i, end="*")
+#     if i > 6:
+#         break
+
+# 0*1*2*3*4*5*6*7*
+
+
+# 🔸 Exercise 2:
+
+# i = 100
+# while i > 0:
+#     if i == 40:
+#         break
+#     print(i, end="*")
+#     i -= 20
+
+# 100*80*60*
+
+# 🔸 Exercise 3:
+
+# n = 10
+# while n > 0:
+#     n -= 1
+#     if n == 2:
+#         continue
+#     print(n, end="*")
+
+# 10*9*8*7*6*5*4*3*1*0*
+
+# 🔸 Exercise 4:
+
+# result = 0
+# for i in range(10):
+#     if i % 2 == 0:
+#         continue
+#     result += i
+
+# print(result)
+
+# 25
+
+# 🔸 Exercise 5:
+
+mult = 1
+for i in range(1, 11):
+    if i % 2 == 0:
+        continue
+    if i % 9 == 0:
+        break
+    mult *= i
+print(mult)
+
+7531
