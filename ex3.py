5e32b4df917ad6fe4cbfe69c211e12e4ed0f0623
Cristofor4ik <xtinabruscaginxxx@gmail.com>
2024-06-26 18:03:23 +0300

    Break. Exercises

---
+++ ex3.py
@@ -0,0 +1,39 @@
+# Exercise 1 🪴
+# Will the else code block in the code snippet below be executed?
+
+# n = 0
+# while n < 10:
+#     n += 2
+#     print(n)
+# else:
+#     print("Цикл завершен.")
+
+# yep
+
+# Exercise 2 🪴
+# Will the else code block in the code snippet below be executed?
+
+# n = 0
+# while n < 10:
+#     n += 2
+#     if n == 8:
+#         break
+#     print(n)
+# else:
+#     print('Цикл завершен.')
+
+# nope
+
+# Exercise 3 🪴
+# Will the else code block in the code snippet below be executed?
+
+# n = 0
+# while n < 10:
+#     n += 2
+#     if n == 7:
+#         break
+#     print(n)
+# else:
+#     print("Цикл завершен.")
+
+# yep
