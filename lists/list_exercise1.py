abef06c8b76770e86d15ff525d16d9fc4910c2a4
Cristofor4ik <xtinabruscaginxxx@gmail.com>
2024-07-13 20:00:38 +0300

    List. Theory

---
+++ lists/list_exercise1.py
@@ -0,0 +1,20 @@
+# Ex 1
+# 💙 The input to the program is one number n. Write a program that prints the list [1, 2, 3, ..., n].
+
+# n = int(input())
+
+# my_list = list(range(1, n + 1))
+# print(my_list)
+
+
+# Ex 2
+# 💙 The input to the program is one number n. Write a program that prints a list of n letters of the English alphabet ['a', 'b', 'c', ...] in lowercase.
+
+# n = int(input())
+# char_list = []
+
+# for ascii_code in range(97, n + 97):
+#     char = [chr(ascii_code)]
+#     char_list += char
+
+# print(char_list)
