f8a2de8650817d469a85fd64f4a3af6c056d2227
Cristofor4ik <xtinabruscaginxxx@gmail.com>
2024-08-01 21:27:38 +0300

    Insertion. Add example

--- sorting/bubble.py
+++ sorting/bubble.py
@@ -26,14 +26,14 @@
 
 # print(a)
 
-a = [5, 2, 9, 1, 5, 6, 8]
+# a = [5, 2, 9, 1, 5, 6, 8]
 
-for i in range(1, len(a)):
-    key = a[i]
-    j = i - 1
-    while j >= 0 and a[j] > key:
-        a[j + 1] = a[j]
-        j -= 1
-    a[j + 1] = key
+# for i in range(1, len(a)):
+#     key = a[i]
+#     j = i - 1
+#     while j >= 0 and a[j] > key:
+#         a[j + 1] = a[j]
+#         j -= 1
+#     a[j + 1] = key
 
-print(a)
+# print(a)
